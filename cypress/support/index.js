1ed4af654c878093ee22701847182cb6bce156a8
ChrisVo87 <chris.vo0505@gmail.com>
2021-10-03 07:59:33 +1100

    4th commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,33 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands';
-
-// Enable support for XPath
-import "cypress-xpath";
+import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import addContext from "mochawesome/addContext";
-
-// Accessibility Testing
-import "cypress-axe";
-
-// API Logging
-import "@bahmutov/cy-api/support";
-
-// Cypress-Plugin-Snapshot
-import "cypress-plugin-snapshots/commands";
-
-Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-});
-
-Cypress.on("test:after:run", (test, runnable) => {
-    if (test.state === "failed") {
-        const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
-        addContext({ test }, screenshot);
-    }
-});
